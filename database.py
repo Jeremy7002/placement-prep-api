56fcd7583cf70d528bbdddefb501566da5aac8de
Jeremy7002 <jerejames1.12@gmail.com>
2026-07-06 19:11:31 +0530

    Add CA cert and connection resiliency for TiDB, update gitignore

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=280)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
